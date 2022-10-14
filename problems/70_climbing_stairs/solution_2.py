f3853ae57b7bc5f4b685bc70fc2845948525b508
PotatCat <olliejmio@gmail.com>
2022-10-14 22:19:59 +1100

    comments

--- problems/70_climbing_stairs/solution_2.py
+++ problems/70_climbing_stairs/solution_2.py
@@ -5,6 +5,9 @@ def climb_stairs(n: int) -> int:
     one, two = 1, 1
 
     for _ in range(n-1):
+
+        # one = one + two & two = one
+        # (doing it this way removes need for temp object)
         one, two = one + two, one
 
     return one
